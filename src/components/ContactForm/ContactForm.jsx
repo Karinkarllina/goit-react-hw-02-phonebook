e251bb372a70d9345502f1dbdb2c347be94b425c
Karinkarllina <karinkhaliullina@gmail.com>
2023-05-12 14:11:04 +0300

    Коригування пропів

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import css from './ContactForm.module.css'
+import css from './ContactForm.module.css';
+import PropTypes from 'prop-types';
 
 export class ContactForm extends Component { 
 
@@ -61,5 +62,9 @@ render() {
     )
 }
 }
+
+ContactForm.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+};
 
 export default ContactForm
